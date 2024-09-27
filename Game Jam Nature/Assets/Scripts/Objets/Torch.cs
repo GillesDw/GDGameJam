2f9ccfc5269c0e56d44f66bb46cb7560fe74866f
GauvinMoens <168551065+GauvinMoens@users.noreply.github.com>
2024-09-27 20:46:58 +0200

    Flashlight Battery charge

--- Game Jam Nature/Assets/Scripts/Objets/Torch.cs
+++ Game Jam Nature/Assets/Scripts/Objets/Torch.cs
@@ -7,6 +7,13 @@ public class Torch : MonoBehaviour
     [SerializeField] GameObject flashlight;
     private bool flashlightTurnOn = false;
     [SerializeField] List<GameObject> flashlightBattery;
+    private float flashlightBatteryTimeLeft;
+    [SerializeField] private float maxFlashlightBatteryTime = 10;
+
+    private void Awake()
+    {
+        flashlightBatteryTimeLeft = maxFlashlightBatteryTime;
+    }
 
     private void Start()
     {
@@ -15,17 +22,73 @@ public class Torch : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (flashlightBatteryTimeLeft > 0)
         {
-            if (flashlightTurnOn != true)
+            if (Input.GetMouseButtonDown(0))
             {
-                flashlight.SetActive(true);
-                flashlightTurnOn = true;
+                if (flashlightTurnOn != true)
+                {
+                    flashlight.SetActive(true);
+                    flashlightTurnOn = true;
+                }
+                else
+                {
+                    flashlight.SetActive(false);
+                    flashlightTurnOn = false;
+                }
             }
-            else
+        }
+
+        if(flashlightTurnOn == true)
+        {
+            flashlightBatteryTimeLeft -= Time.deltaTime;
+        }
+
+        //Flashlight time life
+        if(flashlightBatteryTimeLeft <= 0)
+        {
+            flashlight.SetActive(false);
+            flashlightTurnOn = false;
+        }
+
+        UIBattery();
+    }
+
+    public void UIBattery()
+    {
+        if (flashlightBatteryTimeLeft >= maxFlashlightBatteryTime/4*3)
+        {
+            for (int i = 0; i < flashlightBattery.Count; i++)
             {
-                flashlight.SetActive(false);
-                flashlightTurnOn = false;
+                flashlightBattery[i].SetActive(true);
+            }
+        }
+        if (flashlightBatteryTimeLeft < maxFlashlightBatteryTime / 4 * 3 && flashlightBatteryTimeLeft >= maxFlashlightBatteryTime/2)
+        {
+            flashlightBattery[0].SetActive(true);
+            flashlightBattery[1].SetActive(true);
+            flashlightBattery[2].SetActive(true);
+            flashlightBattery[3].SetActive(false);
+        }
+        if (flashlightBatteryTimeLeft < maxFlashlightBatteryTime / 2 && flashlightBatteryTimeLeft >= maxFlashlightBatteryTime / 4)
+        {
+            flashlightBattery[0].SetActive(true);
+            flashlightBattery[1].SetActive(true);
+            flashlightBattery[2].SetActive(false);
+            flashlightBattery[3].SetActive(false);
+        }
+        if (flashlightBatteryTimeLeft < maxFlashlightBatteryTime / 4 && flashlightBatteryTimeLeft > 0)
+        {
+            flashlightBattery[0].SetActive(true);
+            flashlightBattery[1].SetActive(false);
+            flashlightBattery[2].SetActive(false);
+            flashlightBattery[3].SetActive(false);
+        }
+        if (flashlightBatteryTimeLeft <= 0.1f)
+        {
+            for (int i = 0; i < flashlightBattery.Count; i++)
+            {
+                flashlightBattery[i].SetActive(false);
             }
         }
     }
